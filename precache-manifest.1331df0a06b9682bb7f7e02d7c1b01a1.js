9da7a153a7dcea96dcc543b28cba3bb962bd82d2
Ari Bahtiar <aribahtiar@unsil.ac.id>
2019-11-14 09:36:37 +0700

    starter

---
+++ precache-manifest.1331df0a06b9682bb7f7e02d7c1b01a1.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91924833dbd1c8de2395",
+    "url": "css/app.8dcfa455.css"
+  },
+  {
+    "revision": "f43807acae9f2f2c9e68fd69439c1409",
+    "url": "index.html"
+  },
+  {
+    "revision": "6c8f178e855d836cc77e",
+    "url": "js/4b47640d.607ee95e.js"
+  },
+  {
+    "revision": "91924833dbd1c8de2395",
+    "url": "js/app.05df28b0.js"
+  },
+  {
+    "revision": "eb7b17e80a6b0f0b62e0",
+    "url": "js/fastclick.5251028d.js"
+  },
+  {
+    "revision": "95942f38ac8f361e7467",
+    "url": "js/vendor.8a0e5eba.js"
+  },
+  {
+    "revision": "69586a483c620fe49884cc457e1f9185",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "805b0a66d63db95a76ddda23e3a8c12f",
+    "url": "statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "bde75c95c26cbe407d8c3a51149c9047",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "1e6959a9778d40656217fe5eb7e0b7aa",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "e776afac4479ee2ad8d6a0397661bf09",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "37ab9045dbdc0edf12ea59b3772e19f9",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "65d8571ec508d854ae4132e6540c476a",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "7233d85b329c161211830b1b30fdf0ec",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "c7428f8eba0499407710120fea46ead7",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "04671eaea0c2fc94e31c5ab76ec67d08",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "805b0a66d63db95a76ddda23e3a8c12f",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "3f5de7654b25e5f1640662111d28b490",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "15bc9eca3421a7d5a0599a5fb39fe7a8",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "f309ab32f7ba3a2d3f50d05ba3d2aaa2",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "c8fdfe20437ccd115ee07cb684685ded",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "6624870bb64b1a562e8438e57cdc636a",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "ba58d8b825312a31114fb91484dadbeb",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "81cf4d04261caad3754eaad7cd2cd42f",
+    "url": "statics/img/bank.png"
+  },
+  {
+    "revision": "0abfe801d120daa4747e4c6a81f7f77a",
+    "url": "statics/img/bg.png"
+  },
+  {
+    "revision": "2558ea41ebf5f1365f8651b977c56b16",
+    "url": "statics/img/logo-pesantren.png"
+  },
+  {
+    "revision": "844c8c3debfe0863f153fc66524188b9",
+    "url": "statics/img/logo.png"
+  },
+  {
+    "revision": "2d7a5de0546561b8ef08955497b99c3f",
+    "url": "statics/img/money-bag.png"
+  },
+  {
+    "revision": "c193e98912b0795019ed9e4593e11561",
+    "url": "statics/img/money.png"
+  }
+]);
